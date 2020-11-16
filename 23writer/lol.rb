0c68b5c211f59a495ebe3d8a2aa18ffa799b49c9
stevenacz <scoailazaa@gmail.com>
2020-11-16 17:05:05 -0500

    Problema 23

---
+++ 23writer/lol.rb
@@ -0,0 +1,24 @@
+puts "Enter a multi line response ending with a tab"
+sadasd = gets(":end").chomp
+
+# require_relative "../22document/document"
+
+# option, filename = ARGV
+
+puts "Enter a multi line response ending with a tab"
+sadasd = gets("\t\n").chomp
+# case option
+# when "write"
+#   # response = gets
+#   # File.write(filename, input, mode: "w")
+# when "read"
+
+# when "add"
+
+# when "size"
+
+# when "lines_count"
+
+# when "words_count"
+
+# end
--- 23writer/writer.rb
+++ 23writer/writer.rb
@@ -1,3 +1,33 @@
 require_relative "../22document/document"
 
-# your code here
+option, filename = ARGV
+
+case option
+when "write"
+  puts "Enter a multi line response ending with a tab"
+  input = STDIN.gets(":exit").chomp(":exit")
+  file_title = input.split("\n").last
+  File.write(filename, input, mode: "w")
+  File.rename(filename, "#{file_title}.txt")
+when "read"
+  puts File.read(filename)
+when "add"
+  puts "Enter a multi line response ending with a tab"
+  input = STDIN.gets(":exit").chomp(":exit")
+  file_title = input.split("\n").last
+  File.write(filename, input, mode: "a")
+  File.rename(filename, "#{file_title}.txt")
+when "size"
+  size = File.size(filename)
+  puts "#{filename} size: #{size} bytes"
+when "lines_count"
+  n_lines = 0
+  File.foreach(filename) { n_lines += 1 }
+  puts "#{filename} has #{n_lines} lines"
+when "words_count"
+  n_words = 0
+  File.foreach(filename) do |line|
+    line.chomp.each_char { n_words += 1 }
+  end
+  puts "#{filename} has #{n_words} words"
+end
