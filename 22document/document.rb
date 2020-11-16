3ff48b8de141a4cc439575c2ded24235d9e9cb5b
stevenacz <scoailazaa@gmail.com>
2020-11-16 11:46:02 -0500

    Problema 22

--- 22document/document.rb
+++ 22document/document.rb
@@ -10,25 +10,25 @@ class Document
   def write(input)
     File.write(@filename, input, mode: "w")
   end
-  
+
   def add(input)
     File.write(@filename, input, mode: "a")
   end
-  
+
   def size
     File.size(@filename)
   end
 
   def lines_count
     n_lines = 0
-    File.foreach (@filename) { |line| n_lines += 1 }
+    File.foreach(@filename) { n_lines += 1 }
     n_lines
   end
 
   def words_count
     n_words = 0
-    File.foreach (@filename) do |line|
-      line.chomp.each_char { |word| n_words += 1 }
+    File.foreach(@filename) do |line|
+      line.chomp.each_char { n_words += 1 }
     end
     n_words
   end
